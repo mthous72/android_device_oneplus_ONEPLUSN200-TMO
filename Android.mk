2baca9a43881255cf4c12d11f81f91104b7ac446
mthous72 <mthous01@hotmail.com>
2022-01-26 22:56:48 -0500

    Update Android.mk
    Change to dre8

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), ONEPLUSN200-TMO)
+ifeq ($(TARGET_DEVICE), dre8)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
